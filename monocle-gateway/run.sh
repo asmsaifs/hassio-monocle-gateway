f971eb7d7a2a2646725914619a25f2c99d07a80e
ASM Saiful Islam Chowdhury <asmsaifs@gmail.com>
2023-12-17 22:27:14 +0600

    fix again

--- monocle-gateway/run.sh
+++ monocle-gateway/run.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/with-contenv bashio
+#!/bin/bash
 # Generate monocle.token
-MONOCLE_TOKEN=$(bashio::config 'monocle_token | length')
+MONOCLE_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2YTYyYmQwOC01MTNjLTRmNTItYTdkNC00Njg3ZmIzYTU4M2YiLCJpc3MiOiJtb25vY2xlIiwicm9sZSI6IkFQSSIsInN1YiI6IlU6Y2I0YmZkYjgtOWZkOS00MmVlLWFiMjgtYWFjY2JlNGE2MTM3IiwiYXVkIjoiYXBpIiwiaWF0IjoxNzAyODIyMTEyfQ.Eh3JLR4BcqvbEXWO79HNwlYUnsnudPP4gKGS_ECYOyM"
 if [ "$MONOCLE_TOKEN" -gt "0" ]; then
-   MONOCLE_TOKEN_VALUE=$(bashio::config 'monocle_token')
+   MONOCLE_TOKEN_VALUE=MONOCLE_TOKEN
 
    if [ "$MONOCLE_TOKEN_VALUE" = "-" ] || [ "$MONOCLE_TOKEN_VALUE" = "your_very_long_token" ] ; then       
       exit 1
