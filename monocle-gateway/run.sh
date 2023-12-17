ae169799d07005dffa87a3858ea9810500b5d325
ASM Saiful Islam Chowdhury <asmsaifs@gmail.com>
2023-12-17 22:35:46 +0600

    fix

--- monocle-gateway/run.sh
+++ monocle-gateway/run.sh
@@ -1,14 +1,43 @@
-#!/bin/bash
-# Generate monocle.token
-MONOCLE_TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2YTYyYmQwOC01MTNjLTRmNTItYTdkNC00Njg3ZmIzYTU4M2YiLCJpc3MiOiJtb25vY2xlIiwicm9sZSI6IkFQSSIsInN1YiI6IlU6Y2I0YmZkYjgtOWZkOS00MmVlLWFiMjgtYWFjY2JlNGE2MTM3IiwiYXVkIjoiYXBpIiwiaWF0IjoxNzAyODIyMTEyfQ.Eh3JLR4BcqvbEXWO79HNwlYUnsnudPP4gKGS_ECYOyM"
-if [ "$MONOCLE_TOKEN" -gt "0" ]; then
-   MONOCLE_TOKEN_VALUE=MONOCLE_TOKEN
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# SDeSalve HomeAssistant Addon: Monocle Gateway
+# Monocle Gateway - www.monoclecam.com
+# ==============================================================================
 
-   if [ "$MONOCLE_TOKEN_VALUE" = "-" ] || [ "$MONOCLE_TOKEN_VALUE" = "your_very_long_token" ] ; then       
+bashio::log.green "[Info] Starting addon..."
+
+# Generate monocle.token
+MONOCLE_TOKEN=$(bashio::config 'monocle_token | length')
+if [ "$MONOCLE_TOKEN" -gt "0" ]; then
+   MONOCLE_TOKEN_VALUE=$(bashio::config 'monocle_token')
+
+   if [ "$MONOCLE_TOKEN_VALUE" = "-" ] || [ "$MONOCLE_TOKEN_VALUE" = "your_very_long_token" ] ; then 
+      bashio::log.red "[Error] Invalid config. You'll neend an API Token. Please obtain on https://portal.monoclecam.com/account!"
       exit 1
    fi
 
    rm -f /etc/monocle/monocle.token
    echo "$MONOCLE_TOKEN_VALUE"   > /etc/monocle/monocle.token
-   /usr/local/bin/monocle-gateway
+   bashio::log.green "[Info] monocle.token file was successfully created..."
+             
+   bashio::log.green "[Info] Starting Monocle Gateway service..."
+docker run                           \
+  -it                                \
+  --detach                           \
+  --network host                     \
+  --restart=always                   \
+  --volume /etc/monocle:/etc/monocle \
+  --name=monocle-gateway             \
+  monoclecam/monocle-gateway
+
+echo ">> NEW MONOCLE GATEWAY CONTAINER IS RUNNING"
+echo "------------------------------------------------------------"
+else
+   bashio::log.red \
+     '-----------------------------------------------------------'
+   bashio::log.red '                Oops! Something went wrong.'
+   bashio::log.red
+   bashio::log.red ' monocle_token option is mandatory'
+   bashio::log.red \
+     '-----------------------------------------------------------'
 fi  
